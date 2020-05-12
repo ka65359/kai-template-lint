94ff9056a577e77e70fae67f9e17c8df92a9ebe2
Kai <tehhavok@gmail.com>
2020-05-12 18:21:14 -0400

    Add base eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = exports = {
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "ecmaFeatures": {
+    "jsx": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "jest/globals": true,
+    "cypress/globals": true
+  },
+  "plugins": [
+    "react",
+    "import",
+    "jest",
+    "cypress"
+  ],
+  "rules": {
+    "no-inner-declarations": ["warn", "functions"],
+    "generator-star-spacing": ["warn", {"before": true, "after": false}],
+    "semi": "warn",
+    "semi-style": ["warn", "last"],
+    "no-extend-native": "error",
+    "no-console": ["error", { allow: ["debug", "error", "dir", "info"] }],
+    "no-loop-func": "warn",
+    "no-iterator": "warn",
+    "no-multi-str": "warn",
+    "no-script-url": "warn",
+    "no-undefined": "error",
+    "no-unused-vars": "error",
+    "no-ex-assign": "warn",
+    "no-caller": "error",
+    "no-eval": "warn",
+    "require-yield": "warn",
+    "no-new": "warn",
+    "block-scoped-var": "error",
+    "new-cap": "warn",
+    "wrap-iife": "warn",
+    "arrow-spacing": "warn",
+    "no-empty": ["warn", { "allowEmptyCatch": true }],
+    "react/prop-types": ["off"],
+    "react/display-name": "warn",
+    "eqeqeq": "off"
+  }
+};
